9889d0254ed8259c0ee299b9224a3633f46bb337
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-06-10 17:20:58 +0200

    chore: corregir URL de la documentación de la API en el entorno de producción
    Actualiza la URL de apiDocUrl para apuntar correctamente a la ruta de la documentación de la API.

--- guiders-20/src/environments/environment.prod.ts
+++ guiders-20/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://guiders.ancoradual.com/api',
-  apiDocUrl: 'https://guiders.ancoradual.com/doc',
+  apiDocUrl: 'https://guiders.ancoradual.com/api/doc',
   // URL para la conexión WebSocket en producción
   websocketUrl: 'wss://guiders.ancoradual.com',
   appName: 'Guiders',
