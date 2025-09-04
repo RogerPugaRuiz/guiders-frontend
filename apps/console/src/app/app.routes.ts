c59eb4282af0afea08663ea6e78efa56bae730f5
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-09-04 18:30:00 +0200

    feat(routes): agregar ruta de inicio de sesión y activar guardia de autenticación en la ruta de bandeja de entrada

--- apps/console/src/app/app.routes.ts
+++ apps/console/src/app/app.routes.ts
@@ -4,6 +4,14 @@ export const appRoutes: Route[] = [
 	{
 		path: 'inbox',
 		loadChildren: () => import('@guiders-frontend/chat/features/inbox').then(m => m.inboxRoutes),
+		canActivate: [
+			() => import('@guiders-frontend/auth/features/login').then(m => m.authGuard),
+		],
 		title: 'Chat Inbox',
-	}
+	}, 
+	{
+		path: 'login',
+		loadChildren: () => import('@guiders-frontend/auth/features/login').then(m => m.loginRoutes),
+		title: 'Login',
+	},
 ];
