062a4fdc6f7b2579130406491643970992836531
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-09-05 19:26:08 +0200

    refactor(auth): simplificar authGuard para usar OidcSecurityService y eliminar verificación de token en localStorage

--- libs/auth/features/login/src/lib/auth-guard.ts
+++ libs/auth/features/login/src/lib/auth-guard.ts
@@ -1,19 +1,21 @@
+// auth.guard.ts
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { map, take } from 'rxjs/operators';
 
+export const authGuard: CanActivateFn = (_route, state) => {
+  const oidc = inject(OidcSecurityService);
 
-export const authGuard: CanActivateFn = () => {
-  const oidcSecurityService = inject(OidcSecurityService);
+  return oidc.checkAuth().pipe(
+    take(1),
+    map(({ isAuthenticated }) => {
+      if (isAuthenticated) return true;
 
-  // Verificar si existe el access-token en localStorage
-  const token = localStorage.getItem('access-token');
-  
-  if (token) {
-    return true; // Permitir acceso
-  } else {
-    // Redirigir a login si no hay token
-    oidcSecurityService.authorize();
-    return false;
-  }
+      // Redirige a Keycloak y vuelve a la URL solicitada
+      oidc.authorize();
+
+      return false;
+    })
+  );
 };
