84482a52f7e8cdab5611b708dd6bf855d83b2066
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-09-05 15:22:54 +0200

    feat(routes): reordenar la ruta de inicio de sesión en la configuración de rutas

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -2,11 +2,6 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
 	{
-		path: 'login',
-		loadChildren: () => import('@guiders-frontend/auth/features/login').then(m => m.loginRoutes),
-		title: 'Login',
-	},
-	{
 		path: 'dashboard',
 		loadChildren: () =>
 			import('@guiders-frontend/analytics/features/admin-dashboard').then(m => m.adminDashboardRoutes),
@@ -14,6 +9,11 @@ export const appRoutes: Route[] = [
 		canActivate: [
 			() => import('@guiders-frontend/auth/features/login').then(m => m.authGuard),
 		],
+	},
+	{
+		path: 'login',
+		loadChildren: () => import('@guiders-frontend/auth/features/login').then(m => m.loginRoutes),
+		title: 'Login',
 	},
 	{
 		path: '',
