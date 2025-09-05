577a9416b5a211ebd7f19eff557d7ab9be0606f7
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-09-05 13:48:22 +0200

    feat(routes): agregar rutas para el panel de administración y redirección al dashboard

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -6,4 +6,18 @@ export const appRoutes: Route[] = [
 		loadChildren: () => import('@guiders-frontend/auth/features/login').then(m => m.loginRoutes),
 		title: 'Login',
 	},
+	{
+		path: 'dashboard',
+		loadChildren: () =>
+			import('@guiders-frontend/analytics/features/admin-dashboard').then(m => m.adminDashboardRoutes),
+		canActivate: [
+			() => import('@guiders-frontend/auth/features/login').then(m => m.authGuard),
+		],
+		title: 'Admin Dashboard',
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'dashboard',
+	}
 ];
