21a2f2a1c29e126e4561107c426bbb15ba837116
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-09-05 13:46:57 +0200

    feat(admin-dashboard): agregar nueva biblioteca de panel de administración con rutas, componentes y pruebas

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
+	{
+		path: 'login',
+		loadChildren: () => import('@guiders-frontend/auth/features/login').then(m => m.loginRoutes),
+		title: 'Login',
+	},
 ];
---
+++ libs/analytics/features/admin-dashboard/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { AdminDashboard } from './admin-dashboard/admin-dashboard';
+
+export const adminDashboardRoutes: Route[] = [
+  { path: '', component: AdminDashboard },
+];
